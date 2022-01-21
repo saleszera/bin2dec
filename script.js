eef9a375004121092c71b5add9b2d2af331c94fe
saleszera <crowzcoleslaw@gmail.com>
2022-01-21 00:40:08 -0300

    Add a conversor
    it can convert binary and decimal, everything is doing with javascript. This page is responsive, there are some nice transitions and a kind of a toast 😄<fe0f>

---
+++ script.js
@@ -0,0 +1,70 @@
+const [binaryInput, decimalInput] = document.getElementsByTagName('input')
+const [container] = document.getElementsByClassName('container')
+
+window.onload = function() {
+  binaryInput.focus()
+}
+
+const showToast = (message) => {  
+  const toast = document.createElement('div')
+  const error = document.createElement('p')
+  toast.className = 'toast'
+
+  const errorText = document.createTextNode(message)
+  error.appendChild(errorText)
+  toast.appendChild(error)
+
+  container.parentNode.insertBefore(toast, container)
+
+  setTimeout(() => {
+    toast.remove()
+  }, 2000)
+}
+
+
+const convertBinaryToDecimal = (e) => {
+  const binary = e.target.value.split('')
+
+  if(!binary.length) {    
+    return (decimalInput.value = '')
+  }
+
+  try {    
+    const toDecimal = binary.reduce((acc, curr, index) => {
+      if(curr > 1 || curr < 0){        
+        throw new Error('Entre apenas com números binários!')        
+      }
+      const number = curr * 2 ** index
+  
+      return acc + number
+    }, 0)
+  
+    if(toDecimal){
+      decimalInput.value = toDecimal
+    }   
+  } catch (error) {   
+    showToast(error.message)
+    return (binaryInput.value = '')
+  }
+}
+
+const convertDecimalToBinary = (e) => {
+  let number = e.target.value
+  let numbers = []
+
+
+
+  do {
+    numbers.push(number % 2)
+
+    number = Math.floor(number / 2)
+  } while (number >= 1)
+
+  binaryInput.value = numbers.reverse().join('')
+}
+
+binaryInput.addEventListener('input', e => convertBinaryToDecimal(e))
+decimalInput.addEventListener('input', e => convertDecimalToBinary(e))
+
+binaryInput.removeEventListener('input', convertBinaryToDecimal)
+decimalInput.removeEventListener('input', convertDecimalToBinary)
